5fc2ccf7c7c80a606dcc7a77f20d9cf90e359999
calebstamucsd <cstam@ucsd.edu>
2024-02-27 13:38:19 -0800

    template

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,13 +1,73 @@
 <script>
-  // Write your JS here, or import other files
+import Scroller from "@sveltejs/svelte-scroller";
+
+let count, index, offset, progress;
 </script>
 
-<main>
-  <h1>Svelte template</h1>
+<Scroller
+  top={0.0}
+  bottom={1}
+  threshold={0.5}
+  bind:count
+  bind:index
+  bind:offset
+  bind:progress
+>
+<div class="background" slot="background">
 
-  <p>Write your HTML here</p>
-</main>
+  <div class="progress-bars">
+    <p>current section: <strong>{index + 1}/{count}</strong></p>
+    <progress value={count ? (index + 1) / count : 0} />
+
+    <p>offset in current section</p>
+    <progress value={offset || 0} />
+
+    <p>total progress</p>
+    <progress value={progress || 0} />
+  </div>
+</div>
+
+<div class="foreground" slot="foreground">
+  <section>Section 0: Introduction and CLT Demo (Caleb) </section>
+  <section>Section 1: Law of Large Numbers (Caleb) </section>
+  <section>Section 2: De Moivre Coin Flip Stuff (Kelo) </section>
+  <section>Section 3: Laplace pi in the formula demo (Kelo) </section>
+</div>
+
+</Scroller>
+
 
 <style>
-  /* Write your CSS here */
+  .background {
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    outline: green solid 3px;
+  }
+
+  .foreground {
+    width: 50%;
+    margin: 0 auto;
+    height: auto;
+    position: relative;
+    outline: red solid 3px;
+  }
+
+  .progress-bars {
+    position: absolute;
+    background: rgba(170, 51, 120, 0.2) /*  40% opaque */;
+    visibility: visible;
+  }
+
+  section {
+    height: 80vh;
+    background-color: rgba(0, 0, 0, 0.2); /* 20% opaque */
+    /* color: white; */
+    outline: magenta solid 3px;
+    text-align: center;
+    max-width: 750px; /* adjust at will */
+    color: black;
+    padding: 1em;
+    margin: 0 0 2em 0;
+  }
 </style>
