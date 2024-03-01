9b28535baba98c64b2d824128f3fc6913449520e
calebstamucsd <cstam@ucsd.edu>
2024-03-01 11:41:00 -0800

    text and formatting stuff

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -6,7 +6,9 @@ import { onMount } from 'svelte';
 
 let count, index, offset, progress;
 
-let LLN_sec, height_LLN, width_LLN
+let LLN_sec, height_LLN, width_LLN;
+
+let sectionHeight = '80vh';
 
 // Extra comment so github actions will activate
 
@@ -14,6 +16,11 @@ onMount(() => {
         // Update dimensions when the component is mounted
         width_LLN = LLN_sec.clientWidth;
         height_LLN = LLN_sec.clientHeight;
+
+        const section = document.querySelector('section');
+        if (section) {
+          sectionHeight = `${section.scrollHeight}px`;
+        }
 
         // Update dimensions when the window is resized
         const updateDimensions = () => {
@@ -55,10 +62,15 @@ onMount(() => {
 <div class="foreground" slot="foreground">
   <section>
     Section 0: Introduction and CLT Demo (Caleb) 
+    
   </section>
-  <section bind:this={LLN_sec}>
-    Section 1: Law of Large Numbers (Caleb) 
-    {width_LLN} pixels by {height_LLN} pixels
+  <section style="height: 55vh">
+    Section 1: Law of Large Numbers (Caleb) <br>
+    <p> (Wikipedia Text as Placeholder) In probability theory, the law of large numbers (LLN) is a mathematical theorem that states that the average of the results obtained from a large number of independent and identical random samples converges to the true value, if it exists. More formally, the LLN states that given a sample of independent and identically distributed values, the sample mean converges to the true mean. </p>
+    <p> The Italian mathematician Gerolamo Cardano (1501–1576) stated without proof that the accuracies of empirical statistics tend to improve with the number of trials. This was then formalized as a law of large numbers. A special form of the LLN (for a binary random variable) was first proved by Jacob Bernoulli. It took him over 20 years to develop a sufficiently rigorous mathematical proof which was published in his Ars Conjectandi (The Art of Conjecturing) in 1713. He named this his "Golden Theorem" but it became generally known as "Bernoulli's theorem". This should not be confused with Bernoulli's principle, named after Jacob Bernoulli's nephew Daniel Bernoulli. In 1837, S. D. Poisson further described it under the name "la loi des grands nombres" ("the law of large numbers"). Thereafter, it was known under both names, but the "law of large numbers" is most frequently used. </p>
+    <p> After Bernoulli and Poisson published their efforts, other mathematicians also contributed to refinement of the law, including Chebyshev, Markov, Borel, Cantelli, Kolmogorov and Khinchin. Markov showed that the law can apply to a random variable that does not have a finite variance under some other weaker assumption, and Khinchin showed in 1929 that if the series consists of independent identically distributed random variables, it suffices that the expected value exists for the weak law of large numbers to be true. These further studies have given rise to two prominent forms of the LLN. One is called the "weak" law and the other the "strong" law, in reference to two different modes of convergence of the cumulative sample means to the expected value; in particular, as explained below, the strong form implies the weak. </p>
+  </section>
+  <section bind:this={LLN_sec} style="height: {sectionHeight}">
     <LLN svg_width={width_LLN} svg_height={height_LLN} />
   </section>
   <section>Section 2: De Moivre Coin Flip Stuff (Kelo) </section>
@@ -90,13 +102,21 @@ onMount(() => {
     visibility: visible;
   }
 
+  * {
+    font-family: 'Nunito', sans-serif;
+  }
+
+  p {
+    text-align: justify;
+    text-indent: 50px;
+  }
+
   section {
     height: 80vh;
     background-color: rgba(0, 0, 0, 0.2); /* 20% opaque */
     /* color: white; */
     outline: magenta solid 3px;
     text-align: center;
-    max-width: 750px; /* adjust at will */
     color: black;
     padding: 1em;
     margin: 0 0 2em 0;
